511843f6194bfe0a6a6019df316a9b2716c51025
bloodsbro <vanka2929@gmail.com>
2024-03-15 04:49:32 +0200

    Отправка смс с tinkoff.ru

--- src/modules/sites/tinkoff.ts
+++ src/modules/sites/tinkoff.ts
@@ -1,4 +1,5 @@
 import BaseSite from "./base";
+import Utils from "../utils";
 
 export default class Tinkoff extends BaseSite {
   constructor(context, phone) {
@@ -13,5 +14,43 @@ export default class Tinkoff extends BaseSite {
     }
 
     await this.page.goto('https://www.tinkoff.ru/auth/login/', {waitUntil: "domcontentloaded"});
+
+    await this.page.waitForSelector('[automation-id="phone-input"]');
+    await this.cursor.click('[automation-id="phone-input"]');
+    await this.page.type('[automation-id="phone-input"]', this.phone, {
+      delay: Utils.getRndInteger(global.config.limits.keyboard.delay.min, global.config.limits.keyboard.delay.max)
+    });
+
+    await this.cursor.click('[automation-id="button-submit"]');
+
+    try {
+      await this.page.waitForSelector('[automation-id="otp-input"]', {
+        timeout: Number(global.config.limits.confirm.timeout)
+      });
+    } catch {
+      this.logger.error(`Страница с вводом кода не была открыта, возможно словили ошибку`);
+    }
+
+    while (!this.page.isClosed()) {
+      let delay = Utils.getRndInteger(global.config.limits.resend.min, global.config.limits.resend.max);
+
+      const error = await this.page.evaluate(() => {
+        return '';
+      });
+
+      if (error) {
+        // automation-id="left-time"
+      }
+
+      this.logger.info(`Отправили сообщение, ждём перед повторной отправкой ${Number(delay / 1000)} секунд`);
+      await Utils.sleep(delay);
+
+      try {
+        await this.page.waitForSelector('[automation-id="resend-button"]');
+        await this.cursor.click('[automation-id="resend-button"]');
+      } catch (e) {
+        this.logger.error('Не могу найти кнопку переотправки сообщения');
+      }
+    }
   }
 }
