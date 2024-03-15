e847b7285199ce2dc9068ead49f1b249bcc0f3a8
bloodsbro <vanka2929@gmail.com>
2024-03-15 03:48:48 +0200

    Фикс глобальной области видимости для config

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,7 +1,9 @@
 import AntiCaptcha from "../modules/anticaptcha";
+import Config from "../modules/config";
 
 declare namespace NodeJS {
   interface Global {
     AntiCaptcha: AntiCaptcha;
+    config: typeof Config.get;
   }
 }
